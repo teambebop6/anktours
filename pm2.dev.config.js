a42a96a4b9dd606a2f0dd78e8fe9bd0bc8f73ac3
henryhuang <h1886@outlook.com>
2017-10-21 19:41:51 +0800

    add pm2.dev.config.js

---
+++ pm2.dev.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "ank_dev",
+      script: "./server.js",
+      instances: 2,
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+        DB_NAME: 'ank_dev'
+      },
+      cwd: '/usr/local/share/website/dev.ank-tours.ch/ank_dev'
+    }
+  ],
+};
