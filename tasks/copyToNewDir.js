7ec8f675acd550514e1f02ada822f57374d9bc44
Florian Rudin <flaudre@gmail.com>
2017-11-30 13:40:00 +0800

    Copy images to new dir task

---
+++ tasks/copyToNewDir.js
@@ -0,0 +1,23 @@
+gulp = require('gulp');
+path = require('path');
+
+filename = process.argv[2];
+to = process.argv[3];
+
+console.log("Filename is: " + filename);
+console.log("Destination is: " + to);
+
+dirs = ['./', './squared', './thumbs'];
+
+dirs.forEach(function(dirname){
+  var from = path.join(dirname, filename)
+  console.log("Copying from: " + from);
+
+  var dest = path.join(to, dirname, filename);
+  console.log("To: " + dest);
+  
+  gulp.src(from)
+    .pipe(gulp.dest(dest));
+  
+  console.log("copied image");
+});
