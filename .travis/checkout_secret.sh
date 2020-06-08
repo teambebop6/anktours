8385b9dfb80c776bee911a0e801f94ceee1eb219
henryhuang <h1886@outlook.com>
2020-06-08 16:38:58 +0800

    Try new deploy key

--- .travis/checkout_secret.sh
+++ .travis/checkout_secret.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # Decrypt the private key
-openssl aes-256-cbc -K $encrypted_27e15d652f1d_key -iv $encrypted_27e15d652f1d_iv -in ./.travis/id_rsa_ank_deploy.enc -out id_rsa_ank_deploy -d
+#openssl aes-256-cbc -K $encrypted_27e15d652f1d_key -iv $encrypted_27e15d652f1d_iv -in ./.travis/id_rsa_ank_deploy.enc -out id_rsa_ank_deploy -d
+
+cat $DEPLOY_KEY > deploy_key
 
 # Start SSH agent
 eval $(ssh-agent -s)
 
 # Set the permission of the key
-chmod 600 id_rsa_ank_deploy
+chmod 600 deploy_key
 
 # Add the private key to the system
-ssh-add id_rsa_ank_deploy
+ssh-add deploy_key
 
 git clone git@github.com:teambebop6/anktours-secret.git src/anktours-secret
