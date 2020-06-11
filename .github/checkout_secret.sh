5adc2cb804a89235445e03b7efd957000b54df47
henryhuang <h1886@outlook.com>
2020-06-11 23:33:19 +0800

    debug

--- .github/checkout_secret.sh
+++ .github/checkout_secret.sh
@@ -4,11 +4,16 @@ SECRET_REPO="teambebop6/anktours-secret"
 
 mkdir src/anktours-secret
 
-curl -L -H "Authorization: token ${TOKEN}" "https://api.github.com/repos/${SECRET_REPO}/zipball/master" > secrect.zip
+curl -L -H "Authorization: token ${TOKEN}" "https://api.github.com/repos/${SECRET_REPO}/zipball/master" > secret.zip
 
-unzip secrect.zip -d secrect
+if [ ! -f secret.zip ]; then
+    echo "secret.zip not found!"
+    exit
+fi
 
-cd secrect/* || exit
+unzip secret.zip -d secret
+
+cd secret/* || exit
 
 cp * ../../src/anktours-secret/
 
