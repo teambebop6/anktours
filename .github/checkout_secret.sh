6776a65e5295d0a6ce991f1089d09878114c9a83
henryhuang <h1886@outlook.com>
2020-06-11 23:41:13 +0800

    wget test

--- .github/checkout_secret.sh
+++ .github/checkout_secret.sh
@@ -4,7 +4,7 @@ SECRET_REPO="teambebop6/anktours-secret"
 
 mkdir src/anktours-secret
 
-curl -L -H "Authorization: token ${TOKEN}" "https://api.github.com/repos/${SECRET_REPO}/zipball/master" > secret.zip
+wget --header="Authorization: token ${TOKEN}" https://api.github.com/repos/${SECRET_REPO}/zipball/master -O secret.zip
 
 if [ ! -f secret.zip ]; then
     echo "secret.zip not found!"
